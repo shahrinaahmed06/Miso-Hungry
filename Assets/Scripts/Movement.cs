c5d4edb6467a99b2ae67d2d7f28c8b4ccb561105
syahirahlee <62451840+syahirahlee@users.noreply.github.com>
2020-05-19 00:30:30 +1200

    Player_movement coding
    For user story: 
    As a player, I want to be able to move the avatar so that I can collect items.
    As a player, I want to be able to move the avatar so that I can evade obstacles

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+
+public class Movement
+{
+    public float moveSpeed;
+
+    public Movement(float speed)
+    {
+        moveSpeed = speed;
+
+    }
+    public Vector2 CalcMovement(float h, float v, float deltaTime)
+    {
+      
+        var horizontal_movement = h * moveSpeed * deltaTime;
+        var vertical_movement = v * moveSpeed * deltaTime;
+
+        return new Vector2(horizontal_movement, vertical_movement);
+    }
+    
+
+
+}
