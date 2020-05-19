95839df03ab89f85a3c1b53d9cf83bde67793d2c
shahrinaahmed06 <61952276+shahrinaahmed06@users.noreply.github.com>
2020-05-19 13:11:37 +1200

    This folder contain test files for user stories

---
+++ Tests/PauseMenuTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class PauseMenuTest
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void PausemenuTest()
+        {
+           
+                var go = new GameObject("Pausemenu");
+                Assert.AreEqual("Pausemenu", go.name);//Tests successful execution of pause menu
+
+        }
+
+       [Test]
+        public void PauseButttonTest()
+        {
+
+            var go = new GameObject("PauseButton");
+            Assert.AreEqual("PauseButton", go.name);//Tests successful execution of pause button
+
+        }
+
+
+    }
+}
