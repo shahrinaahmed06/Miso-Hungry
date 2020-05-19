e797063d813afa966cb793551408c2ee8db23ca8
shahrinaahmed06 <61952276+shahrinaahmed06@users.noreply.github.com>
2020-05-20 11:21:59 +1200

    This script contain the codes for Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+/*
+ * Autor: Shahrina Ahmed (wdq1809@aut.ac.nz)
+ * This script load the pause button and pause menu 
+ * while pausing and resuming the game. This also conatin the main menu function
+ * and quiting game to go back to main menu or quit the game after pausing the game.
+ */
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Pausemenu, PauseButton;
+
+    public void Pause()//Pause menu function to activate pause menu and pause the game
+    {
+        Pausemenu.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+
+    public void Resume()//Resume function to get back to game from pause
+    {
+        Pausemenu.SetActive(false);
+        PauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()//manin menu need to be loaded here
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()//Quiting game need to be loaded here
+    {
+        Debug.Log("Quit game...");
+        Application.Quit();
+
+    }
+}
