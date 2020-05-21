bc0936117148211da5a1a7d388808df8ff225873
Vyvyd17 <53415669+Vyvyd17@users.noreply.github.com>
2020-05-22 10:42:35 +1200

    Add files via upload
    Back to main menu button

---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagment;
+
+public class MainMenuButton : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
