e1b924d8290295f6dc39ed8af3b4fc943143c283
DavidHocking2020 <david@hockingfamily.net>
2020-05-24 10:53:48 +1200

    Main Menu System
    Summery
    Main menu
    Level Selection Menu
    Settings Menu
    Apologies for the late upload :)

---
+++ Miso Hungry Main+Settings_Menu/Assets/MuteManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteManager : MonoBehaviour
+{
+    private bool isMuted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isMuted = false;
+    }
+
+    public void mutePressed()
+    {
+        isMuted = !isMuted;
+        AudioListener.pause = isMuted;
+    }
+}
