d17e9dacb1e28b08aad41147a323db267471466e
shahrinaahmed06 <wdg1809@aut.ac.nz>
2020-05-23 13:06:16 +1200

    updated level1

---
+++ Miso Hungry/Assets/Editor/ObstacleTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+    public class ObstacleTest
+    {
+        [Test]
+        public void ObstacleHasVelocity()
+        {
+            
+        }
+
+        [Test]
+        public void BuildObj()
+        {
+           
+        }
+
+        [Test] 
+        public void ObstacleMovement_Horizontal()
+        {
+            
+        }
+
+
+        
+    }
---
+++ Miso Hungry/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawner : MonoBehaviour
+{
+    public float spawnRate = .3f;
+    float nextSpawn = 0f;
+    public GameObject obstacle;
+    public Transform[] spawnPoints;
+
+    void Update()
+    {
+        
+
+        //time since started the game 
+        if (nextSpawn <= Time.time)
+        {
+            SpawnObstacle();
+            nextSpawn = Time.time + spawnRate;
+        }
+    }
+
+    void SpawnObstacle()
+    {
+        int random = Random.Range(0, spawnPoints.Length);
+        Transform spawnPoint = spawnPoints[random];
+        //instantiate obstacle prefab
+        Instantiate(obstacle, spawnPoint.position, spawnPoint.rotation) ;
+    }
+
+}
