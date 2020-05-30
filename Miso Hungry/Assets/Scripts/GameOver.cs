bf977a9c1219d68c36706207742af6895bd8a561
shahrinaahmed06 <wdg1809@aut.ac.nz>
2020-05-31 11:37:43 +1200

    Update with timer and end game scene

---
+++ Miso Hungry/Assets/Scripts/GameOver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public GameObject Gameover;
+    // Start is called before the first frame update
+    public void ReStart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Level1");
+    }
+
+    // Update is called once per frame
+    public void Quit()
+    {
+        Debug.Log("Quit Game...");
+        Application.Quit(); 
+    }
+}
---
+++ Miso Hungry/Assets/Scripts/TimerCountdown.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class TimerCountdown : MonoBehaviour
+{
+
+    public GameObject textDisplay;
+    public int secondsLeft = 20;
+    public bool takingAway = false;
+    void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (takingAway == false && secondsLeft > 0 )
+        {
+            StartCoroutine(TimerTake()); 
+        }
+        
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        if(secondsLeft < 10)
+        {
+            textDisplay.GetComponent<Text>().text = "00:0" + secondsLeft;
+
+        }
+        if (secondsLeft <= 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        
+        }
+
+        else
+        {
+            textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+        }
+        takingAway = false;
+    }
+}
