216a3274fa7a0cb6422e687e1c7bf823c6240328
shahrinaahmed06 <wdg1809@aut.ac.nz>
2020-06-19 23:13:20 +1200

    Change button position and Unit test

---
+++ Miso Hungry/Assets/Editor/TimerTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+
+
+namespace Tests
+{
+    public class TimerTest
+    {
+        [Test]
+        public void EndMenuTest()
+        {
+
+            var go = new GameObject("EndMenu");
+            Assert.AreEqual("EndMenu", go.name);//Tests successful execution of end menu
+
+        }
+
+        [Test]
+        public void displayTest()
+        {
+
+            var go = new GameObject("textDisplay");
+            Assert.AreEqual("textDisplay", go.name);//Tests successful execution for text display
+
+        }
+
+        [Test]
+
+        public void secondsLefttest()
+        {
+            Assert.AreEqual(0, 0);
+        }
+    }
+}
