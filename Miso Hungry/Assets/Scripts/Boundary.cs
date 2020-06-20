807faa4c2abdb2b2f55f37c8979361ec2eaecb6f
syahirahlee <62451840+syahirahlee@users.noreply.github.com>
2020-06-20 15:34:36 +1200

    Fixed player boundary and added unit test
    -player movement does not go out of screen
    -added unit test for playercollisiontest

---
+++ Miso Hungry/Assets/Scripts/Boundary.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundary : MonoBehaviour
+{
+    //this class is to set player's movement is within game screen boundary
+
+    private Vector2 screenBounds;
+    private float objectWidth;
+    private float objectHeight;
+
+    void Start()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        objectWidth = transform.GetComponent<SpriteRenderer>().bounds.extents.x; //extents = size of width / 2
+        objectHeight = transform.GetComponent<SpriteRenderer>().bounds.extents.y; //extents = size of height / 2
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 viewPos = transform.position;
+        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + objectWidth, screenBounds.x - objectWidth);
+        viewPos.y = Mathf.Clamp(viewPos.y, screenBounds.y * -1 + objectHeight, screenBounds.y - objectHeight);
+        transform.position = viewPos;
+    }
+}
